31296eafda97987bbce367eb38e0f541dab5c27f
yang666789 <892940494@qq.com>
2022-10-18 16:02:06 +0800

    第二次提交

--- src/main/java/whut/yy/Singleton/Client.java
+++ src/main/java/whut/yy/Singleton/Client.java
@@ -8,8 +8,12 @@ public class Client {
         MySingleton s2 = MySingleton.getInstance();
         s2.setName("java");
 
+        MySingleton s3 = MySingleton.getInstance();
+        s2.setName("spring");
+
         System.out.println("s1的名字：" + s1.getName());
         System.out.println("s2的名字：" + s2.getName());
+        System.out.println("s3的名字：" + s3.getName());
 
         if (s1 == s2) {
             System.out.println("创建的是同一个实例");
